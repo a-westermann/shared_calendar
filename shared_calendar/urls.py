9fc7a29e65f25ecc1f0213955e5ec96a9332f46d
Vierce1 <a.westermann.19@gmail.com>
2025-04-30 14:18:39 -0500

    url pattern

--- shared_calendar/urls.py
+++ shared_calendar/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.calendar, name='calendar'),
+    path('calendar/', views.calendar, name='calendar'),
     path('api/appointments/', views.create_appointment, name='create_appointment'),
 ] 

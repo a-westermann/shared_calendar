f67d672824e8943bb181342c0316fe7fec9df4ac
Vierce1 <a.westermann.19@gmail.com>
2025-04-30 16:08:20 -0500

    login

--- shared_calendar/settings.py
+++ shared_calendar/settings.py
@@ -1,1 +1,4 @@
-AUTH_USER_MODEL = 'shared_calendar.User' 
+AUTH_USER_MODEL = 'shared_calendar.User'
+LOGIN_URL = '/calendar/login/'
+LOGIN_REDIRECT_URL = '/calendar/'
+LOGOUT_REDIRECT_URL = '/calendar/login/' 

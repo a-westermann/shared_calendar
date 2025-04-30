753e4517e8c3fcde93192ebffa5557f58df1119a
Vierce1 <a.westermann.19@gmail.com>
2025-04-30 15:07:22 -0500

    user to person

--- shared_calendar/models.py
+++ shared_calendar/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class User(models.Model):
+class Person(models.Model):
     first_name = models.CharField(max_length=100)
     password = models.CharField(max_length=100)  # In a real app, we'd use proper password hashing
 
